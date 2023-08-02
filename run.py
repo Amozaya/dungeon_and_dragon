f1a54bea9f03d7fe84065e235fce66114a3b4762
Oleg Fursa <olegfursa93@hotmail.com>
2023-08-02 16:03:58 +0000

    Add chest sacrifice functions

--- run.py
+++ run.py
@@ -8,9 +8,11 @@ class Character:
     """
     def __init__(self):
         self.name = ""
-        self.maxhealth = 20
-        self.current_health = 20
-        self.attack = 1
+        self.maxhealth = 30
+        self.current_health = self.maxhealth
+        self.attack = 5
+        self.armor = 0
+        self.potion = 2
 
 character = Character()
 
@@ -20,9 +22,9 @@ class Dragon:
     """
     def __init__(self):
         self.name = "Dragon"
-        self.maxhealth = 1
-        self.current_health = 1
-        self.attack = 1
+        self.maxhealth = 50
+        self.current_health = 50
+        self.attack = 10
 
 dragon = Dragon()
 
@@ -39,6 +41,12 @@ def start_game():
     """ 
     players_name = input('Please enter your character name:\n')
     player.name = players_name
+    print_pause(f'Your name is {player.name}!')
+    current_player_health()
+    current_player_attack()
+    current_player_armor()
+    current_player_potion()
+    print_pause("")
     intro()
 
 def intro():
@@ -105,11 +113,6 @@ def corridor_selection():
         print('Please select valid option: "Left" or "Right"\n')
         corridor_selection()
 
-def second_room():
-    os.system('clear')
-    print_pause('You are in the second room')
-
-
 def trap_room():
     """
     When player walks into trap room it deals the damage
@@ -121,11 +124,120 @@ def trap_room():
     damage = random.randint(1, 5)    
     print_pause(f'Trap deals {damage} damage')
     player.current_health -= damage
-    print_pause(f'You have {player.current_health} out of {player.maxhealth} health left')
+    current_player_health()
     print_pause('\n')
     print_pause('After escaping the trap you notice this coridor is a dead end.')
     print_pause('You have to return back to the firt room')
     first_room()
+
+
+def second_room():
+    """
+    Gives player an option to open a chest
+    Requires player's input
+    If yes - player will attempt to open chest
+    If no - player will skip the chest
+    """
+    os.system('clear')
+    print_pause('You proceed farther into the dungeon')
+    print_pause('You find yourself in the large room')
+    print_pause('In the middle of the room you see a chest')
+    print_pause('You approach to the chest and see the following writing: \n')
+    print_pause('This chest is cursed. If you wish to open it you need to make a sacrifice.\n')
+
+    option = input('Do you dare to open it? "Y/Yes" to try and open, or any other key to leave the chest alone:\n')
+
+    if option.lower() == 'y' or option.lower() == 'yes':
+        open_chest()
+
+    else:
+        print_pause('You decided not to open the chest')
+        dragon_lair()
+
+
+def open_chest():
+    """
+    Will take an input from the player to make sacrifice
+    Checks for incorrect value and loops until player enters correct value
+    """
+    os.system('clear')
+    print_pause('You decided to open the chest')
+    print_pause('When you trying to open it you hear a voice in your head:\n')
+    print_pause('"If you wish to gain our treasures you must make a sacrifice..."')
+    print_pause('"Some of your health or your potion for our treasuers"')
+    print_pause('"What are you willing to sacrifice?"\n')
+
+    while True:
+        sacrifice_option = input('Will you sacrifice "Health" or "Potion"?:\n')
+        
+        if sacrifice_option.lower() == 'health' or sacrifice_option.lower() == 'potion' :
+            option = sacrifice_option.lower()
+            sacrifice(option)
+            return False
+        else:
+            print_pause('Please chose the correct option: "Health" or "Potion"!')
+
+def sacrifice(option):
+    """
+    Depending on the sacrifice player decided to make
+    player will get some bonus:
+    - for potion - extra attach and armor
+    - for health - extra potion and attack
+    """
+    os.system('clear')
+    print_pause(f'You decided to sacrfice your {option}')
+
+    if option == "health":
+        player.current_health -= 5
+        print_pause('You have sacrificied 5 of your health')
+        current_player_health()
+        player.attack += 3
+        player.potion += 1
+
+        print_pause('As a reward, you receive "+3" to Attack, and "+1" to Potion')
+        current_player_attack()
+        current_player_potion()
+        
+
+    if option == "potion":
+        player.potion -= 1
+        current_player_potion()
+
+        player.armor += 30
+        player.attack += 2
+        print_pause('As a reward, you receive "+2" to Attack, and "+30" to Armor')
+        current_player_attack()
+        current_player_armor()
+    dragon_lair()
+
+def dragon_lair():
+    """
+    Final room where the dragon lives
+    It will introduce player to the dragon and start the fight
+    """
+    os.system('clear')
+    print_pause('You walk away from the chest toward the corridor that leads you to the next room')
+
+
+def current_player_health():
+    #Print player's health stats
+    print_pause(f'Your current health is {player.current_health}')
+
+def current_player_potion():
+    #Print player's potion stats
+    print_pause(f'Your current potion number is {player.potion}')
+
+def current_player_armor():
+    #Print player's armor stats
+    print_pause(f'Your current armor is {player.armor}')
+
+def current_player_attack():
+    #Print player's attack stats
+    print_pause(f'Current attack is {player.attack}')
+
+def current_enemy_health():
+    #Print dragon's health stats
+    print_pause(f'Dragon current health is {enemy.current_health}')
 
 """
 Create global variables to have access to Player character and Dragon enemy attributes
@@ -136,6 +248,6 @@ global enemy
 enemy = dragon
 
 
-trap_room()
+start_game()
 
 
